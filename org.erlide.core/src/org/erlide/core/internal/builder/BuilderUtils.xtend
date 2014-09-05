923be262f0ed95cab495af3c24e66e5393f37d54
Vlad Dumitrescu <vladdu55@gmail.com>
2014-09-05 20:22:08 +0200

    type converter: accept iterables instead of collections

--- org.erlide.core/src/org/erlide/core/internal/builder/BuilderUtils.xtend
+++ org.erlide.core/src/org/erlide/core/internal/builder/BuilderUtils.xtend
@@ -1,39 +1,36 @@
 package org.erlide.core.internal.builder
 
-import com.ericsson.otp.erlang.OtpErlangAtom
 import com.ericsson.otp.erlang.OtpErlangObject
 import com.ericsson.otp.erlang.OtpErlangString
 import java.util.Collection
 import org.erlide.core.builder.CompilerOptions
 import org.erlide.engine.model.root.IErlProject
-import org.erlide.util.erlang.OtpErlang
+import org.erlide.util.erlang.ErlUtils
 
 class BuilderUtils {
-    def static OtpErlangObject createProjectInfo(IErlProject project) {
+	def static OtpErlangObject createProjectInfo(IErlProject project) {
 
-        // TODO make sure path variables are replaced!
-        val props = project.properties
-        val rootDir = project.workspaceProject.location.toPortableString
-        val srcDirs = props.sourceDirs
-        val incDirs = props.includeDirs
-        val outDir = props.outputDir
-        val opts = CompilerOptions.get(project.workspaceProject)
-        val minOtpVsn = props.requiredRuntimeVersion.asMajor
-        val Collection<String> libs = newArrayList // TODO get libs from externalModules
-        val Collection<String> libIncs = newArrayList // TODO get lib incs from externalIncludes
+		// TODO make sure path variables are replaced!
+		val props = project.properties
+		val rootDir = project.workspaceProject.location.toPortableString
+		val srcDirs = props.sourceDirs
+		val incDirs = props.includeDirs
+		val outDir = props.outputDir
+		val opts = CompilerOptions.get(project.workspaceProject)
+		val minOtpVsn = props.requiredRuntimeVersion.asMajor
+		val Collection<String> libs = newArrayList // TODO get libs from externalModules
+		val Collection<String> libIncs = newArrayList // TODO get lib incs from externalIncludes
 
-        val Collection<OtpErlangObject> result = newArrayList(
-            new OtpErlangAtom("project_info"),
-            new OtpErlangString(rootDir),
-            OtpErlang.mkList(srcDirs.map[new OtpErlangString(it.toPortableString)]),
-            OtpErlang.mkList(
-                incDirs.map[new OtpErlangString(it.toPortableString)] +
-                    libIncs.map[new OtpErlangString(it)]),
-            new OtpErlangString(outDir.toPortableString),
-            opts,
-            new OtpErlangString(minOtpVsn.toString),
-            OtpErlang.mkList(libs.map[new OtpErlangString(it)])
-        )
-        return OtpErlang.mkTuple(result)
-    }
+		ErlUtils.format(
+			"{project_info,~s,~ls,~ls,~s,~x,~s,~ls}",
+			rootDir,
+			srcDirs.map[new OtpErlangString(it.toPortableString)],
+			incDirs.map[new OtpErlangString(it.toPortableString)] +
+				libIncs.map[new OtpErlangString(it)],
+			outDir.toPortableString,
+			opts,
+			minOtpVsn.toString,
+			libs.map[new OtpErlangString(it)]
+		)
+	}
 }
--- org.erlide.core/xtend-gen/org/erlide/core/internal/builder/BuilderUtils.java
+++ org.erlide.core/xtend-gen/org/erlide/core/internal/builder/BuilderUtils.java
@@ -1,6 +1,5 @@
 package org.erlide.core.internal.builder;
 
-import com.ericsson.otp.erlang.OtpErlangAtom;
 import com.ericsson.otp.erlang.OtpErlangList;
 import com.ericsson.otp.erlang.OtpErlangObject;
 import com.ericsson.otp.erlang.OtpErlangString;
@@ -9,68 +8,69 @@ import java.util.Collection;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.xtext.xbase.lib.CollectionLiterals;
-import org.eclipse.xtext.xbase.lib.Conversions;
+import org.eclipse.xtext.xbase.lib.Exceptions;
 import org.eclipse.xtext.xbase.lib.Functions.Function1;
 import org.eclipse.xtext.xbase.lib.IterableExtensions;
 import org.erlide.core.builder.CompilerOptions;
 import org.erlide.engine.model.root.ErlangProjectProperties;
 import org.erlide.engine.model.root.IErlProject;
 import org.erlide.runtime.runtimeinfo.RuntimeVersion;
-import org.erlide.util.erlang.OtpErlang;
+import org.erlide.util.erlang.ErlUtils;
 
 @SuppressWarnings("all")
 public class BuilderUtils {
   public static OtpErlangObject createProjectInfo(final IErlProject project) {
-    final ErlangProjectProperties props = project.getProperties();
-    IProject _workspaceProject = project.getWorkspaceProject();
-    IPath _location = _workspaceProject.getLocation();
-    final String rootDir = _location.toPortableString();
-    final Collection<IPath> srcDirs = props.getSourceDirs();
-    final Collection<IPath> incDirs = props.getIncludeDirs();
-    final IPath outDir = props.getOutputDir();
-    IProject _workspaceProject_1 = project.getWorkspaceProject();
-    final OtpErlangList opts = CompilerOptions.get(_workspaceProject_1);
-    RuntimeVersion _requiredRuntimeVersion = props.getRequiredRuntimeVersion();
-    final RuntimeVersion minOtpVsn = _requiredRuntimeVersion.asMajor();
-    final Collection<String> libs = CollectionLiterals.<String>newArrayList();
-    final Collection<String> libIncs = CollectionLiterals.<String>newArrayList();
-    OtpErlangAtom _otpErlangAtom = new OtpErlangAtom("project_info");
-    OtpErlangString _otpErlangString = new OtpErlangString(rootDir);
-    final Function1<IPath, OtpErlangString> _function = new Function1<IPath, OtpErlangString>() {
-      public OtpErlangString apply(final IPath it) {
-        String _portableString = it.toPortableString();
-        return new OtpErlangString(_portableString);
+    try {
+      OtpErlangObject _xblockexpression = null;
+      {
+        final ErlangProjectProperties props = project.getProperties();
+        IProject _workspaceProject = project.getWorkspaceProject();
+        IPath _location = _workspaceProject.getLocation();
+        final String rootDir = _location.toPortableString();
+        final Collection<IPath> srcDirs = props.getSourceDirs();
+        final Collection<IPath> incDirs = props.getIncludeDirs();
+        final IPath outDir = props.getOutputDir();
+        IProject _workspaceProject_1 = project.getWorkspaceProject();
+        final OtpErlangList opts = CompilerOptions.get(_workspaceProject_1);
+        RuntimeVersion _requiredRuntimeVersion = props.getRequiredRuntimeVersion();
+        final RuntimeVersion minOtpVsn = _requiredRuntimeVersion.asMajor();
+        final Collection<String> libs = CollectionLiterals.<String>newArrayList();
+        final Collection<String> libIncs = CollectionLiterals.<String>newArrayList();
+        final Function1<IPath, OtpErlangString> _function = new Function1<IPath, OtpErlangString>() {
+          public OtpErlangString apply(final IPath it) {
+            String _portableString = it.toPortableString();
+            return new OtpErlangString(_portableString);
+          }
+        };
+        Iterable<OtpErlangString> _map = IterableExtensions.<IPath, OtpErlangString>map(srcDirs, _function);
+        final Function1<IPath, OtpErlangString> _function_1 = new Function1<IPath, OtpErlangString>() {
+          public OtpErlangString apply(final IPath it) {
+            String _portableString = it.toPortableString();
+            return new OtpErlangString(_portableString);
+          }
+        };
+        Iterable<OtpErlangString> _map_1 = IterableExtensions.<IPath, OtpErlangString>map(incDirs, _function_1);
+        final Function1<String, OtpErlangString> _function_2 = new Function1<String, OtpErlangString>() {
+          public OtpErlangString apply(final String it) {
+            return new OtpErlangString(it);
+          }
+        };
+        Iterable<OtpErlangString> _map_2 = IterableExtensions.<String, OtpErlangString>map(libIncs, _function_2);
+        Iterable<OtpErlangString> _plus = Iterables.<OtpErlangString>concat(_map_1, _map_2);
+        String _portableString = outDir.toPortableString();
+        String _string = minOtpVsn.toString();
+        final Function1<String, OtpErlangString> _function_3 = new Function1<String, OtpErlangString>() {
+          public OtpErlangString apply(final String it) {
+            return new OtpErlangString(it);
+          }
+        };
+        Iterable<OtpErlangString> _map_3 = IterableExtensions.<String, OtpErlangString>map(libs, _function_3);
+        _xblockexpression = ErlUtils.format(
+          "{project_info,~s,~ls,~ls,~s,~x,~s,~ls}", rootDir, _map, _plus, _portableString, opts, _string, _map_3);
       }
-    };
-    Iterable<OtpErlangString> _map = IterableExtensions.<IPath, OtpErlangString>map(srcDirs, _function);
-    OtpErlangList _mkList = OtpErlang.mkList(((OtpErlangObject[])Conversions.unwrapArray(_map, OtpErlangObject.class)));
-    final Function1<IPath, OtpErlangString> _function_1 = new Function1<IPath, OtpErlangString>() {
-      public OtpErlangString apply(final IPath it) {
-        String _portableString = it.toPortableString();
-        return new OtpErlangString(_portableString);
-      }
-    };
-    Iterable<OtpErlangString> _map_1 = IterableExtensions.<IPath, OtpErlangString>map(incDirs, _function_1);
-    final Function1<String, OtpErlangString> _function_2 = new Function1<String, OtpErlangString>() {
-      public OtpErlangString apply(final String it) {
-        return new OtpErlangString(it);
-      }
-    };
-    Iterable<OtpErlangString> _map_2 = IterableExtensions.<String, OtpErlangString>map(libIncs, _function_2);
-    Iterable<OtpErlangString> _plus = Iterables.<OtpErlangString>concat(_map_1, _map_2);
-    OtpErlangList _mkList_1 = OtpErlang.mkList(((OtpErlangObject[])Conversions.unwrapArray(_plus, OtpErlangObject.class)));
-    String _portableString = outDir.toPortableString();
-    OtpErlangString _otpErlangString_1 = new OtpErlangString(_portableString);
-    String _string = minOtpVsn.toString();
-    OtpErlangString _otpErlangString_2 = new OtpErlangString(_string);
-    final Function1<String, OtpErlangString> _function_3 = new Function1<String, OtpErlangString>() {
-      public OtpErlangString apply(final String it) {
-        return new OtpErlangString(it);
-      }
-    };
-    Iterable<OtpErlangString> _map_3 = IterableExtensions.<String, OtpErlangString>map(libs, _function_3);
-    OtpErlangList _mkList_2 = OtpErlang.mkList(((OtpErlangObject[])Conversions.unwrapArray(_map_3, OtpErlangObject.class)));
-    final Collection<OtpErlangObject> result = CollectionLiterals.<OtpErlangObject>newArrayList(_otpErlangAtom, _otpErlangString, _mkList, _mkList_1, _otpErlangString_1, opts, _otpErlangString_2, _mkList_2);
-    return OtpErlang.mkTuple(((OtpErlangObject[])Conversions.unwrapArray(result, OtpErlangObject.class)));
+      return _xblockexpression;
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
   }
 }
